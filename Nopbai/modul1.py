f349a20282ad8f27ebfb29a104d21cbd8be267b0
DTH235655-NGUYENTRUNGHIEU <nthieu11052005@gmail.com>
2025-08-29 10:28:14 +0700

    answer

---
+++ Nopbai/modul1.py
@@ -0,0 +1,15 @@
+#module1.py
+question = 'what is the meaning of life, the unverse and everything?'
+answer = 42
+
+#module1.py
+question = 'what is your quest?'
+asswrt = 'to seek the holy grail.'
+
+import module1
+import modul2
+print(module1.question)
+print(modul2.question)
+print(module1.answer)
+print(modul2.answer)
+
