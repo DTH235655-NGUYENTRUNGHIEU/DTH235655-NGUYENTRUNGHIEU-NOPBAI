ff6fcff72e2b6abf5552ba92f97a29821bdbdf63
DTH235655-NGUYENTRUNGHIEU <nthieu11052005@gmail.com>
2025-09-04 12:39:24 +0700

    3kiudl

---
+++ chương2/3kiudl.py
@@ -0,0 +1,8 @@
+str = 'Trường Dai Hoc An Giang'
+n = 100
+Hesl= 2.34
+print('kiu du liu cua bien str: ', type(str))
+print('kiu du liu cua bien n: ', type(n))
+print('kiu du liu cua bien Hesl: ', type(Hesl))
+print('kiu du liu cua pheo toan so sanh 1 > 0: ', type(1>0))
+PI = 3.14159
