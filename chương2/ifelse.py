71fae108832926b757e50317cc1080f10bfd0da1
DTH235655-NGUYENTRUNGHIEU <nthieu11052005@gmail.com>
2025-09-04 13:41:03 +0700

    ifelse

---
+++ chương2/ifelse.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+if a < b:
+    print('a= ',a)
+    print('b = ', b)
+    print('a nho hon b')
+else:
+    print('a= ', a)
+    print('b= ', b)
+    print('a lon hon b')
+print('tong cua a va b: ',a+b) 
