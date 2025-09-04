26c6e58dfc08629f9df76c11aff089fa76560f6b
DTH235655-NGUYENTRUNGHIEU <nthieu11052005@gmail.com>
2025-09-05 01:17:11 +0700

    baitapcau18

---
+++ chương2/baitapcau18.py
@@ -0,0 +1,24 @@
+import math
+#a
+def demuocnt(n):
+    uocnguyento = set()
+    for i in range(2,int(math.sqrt(n)+1)):
+        while n % i ==0:
+            uocnguyento.add(i)
+            n = n // i
+    if n > 1:
+        uocnguyento.add(n)
+    return len(uocnguyento)
+#b
+def uocsots(n):
+    tong = 0
+    for i in range(1,n):
+        if n % i == 0:
+            tong = tong + i
+    return tong
+#c
+n = int(input('nhap n: '))
+print('số lượng ước nguyên tố: ',demuocnt(n))
+print('tong uoc so: ',uocsots(n))
+
+    
