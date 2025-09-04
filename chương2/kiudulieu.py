151765bb5453df40117547b0cd3ca5bdb488e9ce
DTH235655-NGUYENTRUNGHIEU <nthieu11052005@gmail.com>
2025-09-04 12:31:16 +0700

    kiudulieu

---
+++ chương2/kiudulieu.py
@@ -0,0 +1,6 @@
+#khai bao và gan gia tri so nguyen bang 49 cho bien siso 
+siso = 40
+#hien thi gia tri bien siso ra man hinh 
+print('sĩ số lớp: ',siso)
+#hien thi kiu du lieu cua bien siso
+print('kiu du lieu: ',type(siso))
