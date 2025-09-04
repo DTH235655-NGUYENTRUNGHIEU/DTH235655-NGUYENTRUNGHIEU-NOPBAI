9ad4f47647776c9bba8a3db1dc39fb99cdc1b287
DTH235655-NGUYENTRUNGHIEU <nthieu11052005@gmail.com>
2025-09-04 12:50:25 +0700

    cachambiendoi

---
+++ chương2/cachambiendoi.py
@@ -0,0 +1,15 @@
+#ham int() bien doi so thuc, chuoi thanh so nguyen
+print(int(3.14))
+print(int(3.9999))
+print(int(3.0)) # khong lam tron toi so nguyen gan nhat
+print(int(-3.9999))
+print(int('1234'))
+
+# ham float() bien doi so nguyen, chuoi thanh so thuc
+print(float(17))
+print(float('123.45'))
+
+# ham str() bien doi doi so cua nos thanh chuoi
+print(str(17))
+print(str('123.45'))
+
