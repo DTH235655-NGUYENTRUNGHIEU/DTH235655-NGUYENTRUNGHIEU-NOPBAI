d8afae160bc38f8007b685e4d21fbfe780549183
DTH235655-NGUYENTRUNGHIEU <nthieu11052005@gmail.com>
2025-09-09 14:50:56 +0700

    cau9

---
+++ chuong3/cau9.py
@@ -0,0 +1,13 @@
+n = int(input("nhap vao 1 tháng 1<=n<=12: "))
+if n == 1 or  n == 2 or n ==3:
+    print("quy 1")
+elif n == 4 or  n == 5 or n ==6:
+    print("quy 2")
+elif n == 7 or  n == 8 or n ==9:
+    print("quy 3")
+elif n == 10 or  n == 11 or n ==12:
+    print("quy 4")
+else:
+    print("hông có tháng này!!")
+
+    
