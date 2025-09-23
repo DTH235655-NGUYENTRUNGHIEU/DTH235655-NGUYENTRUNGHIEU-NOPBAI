ccda337a8331747d089c3d28cb1d5d4018d122eb
DTH235655-NGUYENTRUNGHIEU <nthieu11052005@gmail.com>
2025-09-23 14:54:20 +0700

    cau7

---
+++ chuong4/cau7.py
@@ -0,0 +1,11 @@
+from math import sqrt
+Xa = float(input("nhập Xa: "))
+Ya = float(input("nhập Ya: "))
+
+Xb = float(input("nhập Xb: "))
+Yb = float(input("nhập Yb: "))
+
+kq = sqrt(((Xb-Xa)**2) + ((Yb-Ya)**2))
+print("độ dài đoạn AB: ",kq)
+
+
