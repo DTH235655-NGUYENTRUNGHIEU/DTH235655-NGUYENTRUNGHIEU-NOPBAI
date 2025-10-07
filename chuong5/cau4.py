a2e4eda6262941fd4724cc95439807a0dab92ae8
DTH235655-NGUYENTRUNGHIEU <nthieu11052005@gmail.com>
2025-10-07 14:34:17 +0700

    cau4

---
+++ chuong5/cau4.py
@@ -0,0 +1,9 @@
+#Hàm	Ý nghĩa	Ví dụ	Kết quả
+#len(s)	Trả về độ dài chuỗi	len("Hello")	5
+#s.lower()	Chuyển về chữ thường	"Hello".lower()	"hello"
+#s.upper()	Chuyển về chữ hoa	"Hello".upper()	"HELLO"
+#s.capitalize()	Viết hoa chữ đầu tiên	"hiêu học python".capitalize()	"Hiêu học python"
+#s.title()	Viết hoa chữ cái đầu mỗi từ	"hiêu học python".title()	"Hiêu Học Python"
+#s.strip()	Xóa khoảng trắng đầu & cuối chuỗi	" abc ".strip()	"abc"
+#s.lstrip()	Xóa khoảng trắng bên trái	" abc".lstrip()	"abc"
+#s.rstrip()	Xóa khoảng trắng bên phải	"abc ".rstrip()	"abc"/
